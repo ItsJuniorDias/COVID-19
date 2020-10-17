685f6624d076e40e3ccf7e782ec1761780cc7887
Junior Dias <juniordias_@live.com>
2020-10-17 14:12:38 -0300

    chore: Eslint, prettier and EditorConfig

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+};
